1f75727c9c121df5814a79a499a25b370f342a3e
Jordi Collell <jordic@gmail.com>
2016-09-25 12:33:10 +0200

    component.dialog

--- src/app/shared/dialog/dialog.component.ts
+++ src/app/shared/dialog/dialog.component.ts
@@ -11,13 +11,48 @@ import {state,
 @Component({
   selector: "ti-dialog",
   templateUrl:"./dialog.component.html",
-  styleUrls: ["./dialog.component.scss"]
-})
+  styleUrls: ["./dialog.component.scss"],
+  host: {
+      '[@flyInOut]': 'state'
+  },
+  animations: [
+    trigger('flyInOut', [
+      state('in', style({transform: 'translate3d(-50%, -50%, 0)'})),
+      transition('void => *', [
+        style({transform: 'translate3d(-50%, -200%, 0)'}),
+        animate('1s ease-in-out')
+      ]),
+      transition('* => void', [
+        style({transform: 'translate3d(-50%, 200%, 0)'}),
+        animate('600ms ease-in')
+      ])
+    ]),
+    trigger('delayed', [
+      transition('void => *', [
+        style({top: '-300px'}),
+        animate('600ms 200ms ease-in-out')
+      ]),
+    ]),
+    trigger('delayed2', [
+      transition('void => *', [
+        style({top: '-300px'}),
+        animate('600ms 400ms ease-in-out')
+      ]),
+    ])
 
+  ]
+})
 
 
 export class TiDialogComponent implements OnInit {
 
+  @Input() public title:string;
+  @Input() public showButton:boolean;
+  @Input() public button:string;
+
+  public state: string = "in";
+
   ngOnInit() {}
+
 
 }
