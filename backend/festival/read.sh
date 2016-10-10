73afe43887835a7881e4ee023e76d0c745f08b06
Jordi Collell <jordic@gmail.com>
2016-10-10 13:48:39 +0200

    Canvi de encoder de lame a ffmpeg

--- backend/festival/read.sh
+++ backend/festival/read.sh
@@ -9,5 +9,7 @@ TEXT=$(echo $1 | iconv -f utf-8 -t ISO-8859-15//TRANSLIT//IGNORE)
 shift;
 shift;
 
-#echo $TEXT | text2wave -eval "(voice_upc_ca_ona_hts)" $@ | lame - $OUTPUT
-echo $TEXT | text2wave -eval "(voice_upc_ca_ona_hts)" $@ > $OUTPUT
+#echo $TEXT | text2wave -eval "(voice_upc_ca_ona_hts)" $@ | lame --nores - $OUTPUT
+echo $TEXT | text2wave -eval "(voice_upc_ca_ona_hts)" $@ > $OUTPUT.raw
+
+ffmpeg -i $OUTPUT.raw -vn -ar 44100 -ac 2 -ab 128k -f mp3 $OUTPUT
