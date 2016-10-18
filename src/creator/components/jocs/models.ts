9cacbc2af2da6d1e30041dbcabc608a38182cfb2
Jordi Collell <jordic@gmail.com>
2016-10-18 06:22:52 +0200

    afegeixo contingut de tots els jocs a l'editor

--- src/creator/components/jocs/models.ts
+++ src/creator/components/jocs/models.ts
@@ -52,17 +52,31 @@ export const tipusJoc:TJoc[] = [
     label: "Memory",
     validate: joc => joc.words.length==8,
     validateHelp: joc => `Necessites definir 8 paraules.
-      En tens ${joc.words.length}.`
+      Tens ${joc.words.length} paraules.`
   },
   {
     desc: "El joc de les parelles on s'ha de relacionar imatge i texte",
     key: "relaciona",
-    label: "Relaciona"
+    label: "Relaciona",
+    validate: joc => joc.words.length>=3,
+    validateHelp: joc => `Necessites definir un mínim de 3 paraules.
+      Tens ${joc.words.length} paraules.`
   },
   {
     desc: "Tens les lletres de la paraula desordenades i les has d'ordenar",
     key: "sequencing",
-    label: "Escriure Paraules"
+    label: "Escriure Paraules",
+    validate: joc => joc.words.length>=3,
+    validateHelp: joc => `Necessites definir un mínim de 3 paraules.
+      Tens ${joc.words.length} paraules.`
+  },
+  {
+    desc: "Et proposa 4 paraules, i només una és la bona.",
+    key: "mixed",
+    label: "Paraules barrejades",
+    validate: joc => joc.words.length>=3,
+    validateHelp: joc => `Necessites definir un mínim de 3 paraules.
+      Tens ${joc.words.length} paraules.`
   }
 ]
 
