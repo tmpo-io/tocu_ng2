4df0448b2e85ed131584b3280fae7011caa3f571
Jordi Collell <jordic@gmail.com>
2016-10-22 07:00:50 +0200

    nova versio del backend deployada a digital ocean

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: false,
-  wordsEndpoint: '/assets/animals.json',
-  translatorEndpoint: "http://localhost:8080/translate",
-  clipArtEndpoint: "https://openclipart.org/search/json/?amount=40&sort=favorites&query=",
-  audioEndpoint: "http://localhost:8080/audio"
-}
+  wordsEndpoint: "/assets/animals.json",
+  translatorEndpoint: "http://apijocs.tmpo.io/translate",
+  clipArtEndpoint: "https://openclipart.org/search/json/?amount=40&sort=downloads&query=",
+  audioEndpoint: "http://apijocs.tmpo.io/audio"
+
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@
 export const environment = {
   production: false,
   wordsEndpoint: "/assets/animals.json",
-  translatorEndpoint: "http://localhost:8080/translate",
+  translatorEndpoint: "http://apijocs.tmpo.io/translate",
   clipArtEndpoint: "https://openclipart.org/search/json/?amount=40&sort=downloads&query=",
-  audioEndpoint: "http://localhost:8080/audio"
+  audioEndpoint: "http://apijocs.tmpo.io/audio"
 
 };
