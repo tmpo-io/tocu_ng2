dbac90ef840fbba2b41713e238967e09271e64b0
jordi <jordic@gmail.com>
2016-10-27 12:54:16 +0200

    cambis

--- src/game/dashboard.effects.ts
+++ src/game/dashboard.effects.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 
 import { Actions, Effect } from '@ngrx/effects';
-import { Store, Action } from '@ngrx/store';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 
 import { AngularFire } from 'angularfire2';
-
-
 import { AuthService } from  '../auth/services/auth-service';
 
 import { Dashboard } from '../models/dashboard';
 import { DashboardActions } from './dashboard.actions';
+
+import { cleanObject } from '../shared/utils';
 
 import 'rxjs/add/operator/ignoreElements';
 
@@ -47,7 +47,7 @@ export class DashboardEffects {
     return this.a$
       .ofType(DashboardActions.DASH_DATALOAD)
       .switchMap(() =>
-        this.getGames()
+        this.getGames().take(1)
         .map(g => DashboardActions.loadDataOk(g))
       );
   }
@@ -56,14 +56,21 @@ export class DashboardEffects {
   updateDashboard$() {
     return this.a$
       .ofType(DashboardActions.DASH_UPDATE)
-      .delay(2000)
-      .switchMap(() => {
-        this.getSetupObject().set(true);
-        //@TODO Copy available games...
-        return Observable.of(
-          DashboardActions.updateBoardOk(),
-          DashboardActions.loadData()
-        );
+      .delay(500)
+      .do(() => {
+        let user = this.getGames();
+        this.getStarterDb()
+          .map((item) => {
+            item.map((a) => {
+              user.push(cleanObject(a));
+            });
+          });
+      }).map((a) => {
+          this.getSetupObject().set(true);
+          return Observable.of(
+            DashboardActions.updateBoardOk(),
+            DashboardActions.loadData()
+          );
       });
   }
 
@@ -74,6 +81,11 @@ export class DashboardEffects {
 
   get uid(): string {
     return this.auth.id;
+  }
+
+  getStarterDb() {
+    return this
+      .db.list('starter');
   }
 
   getSetupObject() {
@@ -89,7 +101,7 @@ export class DashboardEffects {
 
   getGames() {
     return this.db
-      .list(`users/${this.uid}/jocs/`).take(1);
+      .list(`users/${this.uid}/jocs/`);
   }
 
 }
--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,4 +1,10 @@
 
 
-import { Observable } from 'rxjs/Observable';
+// import { Observable } from 'rxjs/Observable';
 
+
+export function cleanObject(obj) {
+  delete obj.$key;
+  delete obj.$exists;
+  return obj;
+}
