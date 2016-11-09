7902d7e6b68de90b872c6e378359f8b0cc1d5071
Jordi Collell <jordic@gmail.com>
2016-11-09 06:19:57 +0100

    Created a route to test components isolateds
    Page test.component

---
+++ src/game/pages/test.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template:  `<balloons amount="20" delay="3000">
+    </balloons>
+    `
+
+})
+export class TestComponent {
+  constructor() { }
+
+}
