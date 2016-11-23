058df1848903685cb144dd7a1b95704cea889e81
jordi <jordic@gmail.com>
2016-11-23 11:21:56 +0100

    lint

--- src/app/creator/services/openclipart.service.ts
+++ src/app/creator/services/openclipart.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Headers, Http, Response } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs';
 
 import { environment } from '../../../environments/environment';
@@ -9,50 +9,46 @@ const oClipart = environment.clipArtEndpoint;
 
 
 export class Clipart {
-  public thumb:string
-  public url:string
-  public score:number
-  public downloaded:number
+  public thumb: string;
+  public url: string;
+  public score: number;
+  public downloaded: number;
 }
 
 export class Translated {
-  public catala:string
-  public castellano:string
-  public english:string
+  public catala: string;
+  public castellano: string;
+  public english: string;
 }
 
 
 @Injectable()
 export class OpenClipartService {
 
-  constructor(private http:Http) {
-
-  }
+  constructor(private http: Http) {}
 
   getCliparts(word): Observable<Clipart[]> {
-
-    const ffilter = function(r:Response) {
-      let res:Clipart[] = [];
-      r.json().payload.forEach((item)=>{
+    const ffilter = function (r: Response) {
+      let res: Clipart[] = [];
+      r.json().payload.forEach((item) => {
         res.push({
           thumb: item.svg.png_thumb,
           url: item.svg.png_full_lossy,
           score: item.total_favorites,
           downloaded: item.downloaded_by
         });
-      })
+      });
       return Observable.from(res);
-    }
-
-    let compare = function(a,b) {
-      if (a.downloaded_by < b.downloaded_by)
+    };
+    let compare = function (a, b) {
+      if (a.downloaded_by < b.downloaded_by) {
         return -1;
-      if (a.downloaded_by > b.downloaded_by)
+      }
+      if (a.downloaded_by > b.downloaded_by) {
         return 1;
+      }
       return 0;
-    }
-
-
+    };
     return this.http
       .get(translator + `?w=${word}`)
       .flatMap((r: Response) => {
@@ -63,19 +59,18 @@ export class OpenClipartService {
           .flatMap(ffilter);
 
         // Start english request
-        let b =  this.http
+        let b = this.http
           .get(oClipart + m.english)
           .flatMap(ffilter);
 
         return a.concat(b)
-          .distinct((x,y) => {
+          .distinct((x, y) => {
             return x.url == y.url;
           })
           .toArray()
-          .map(arr=>arr.sort(compare));
+          .map(arr => arr.sort(compare));
 
-    })
+      });
   }
-
 
 }
